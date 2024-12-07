c1aa38ebecb2a915f6adb2f9cabcc7ca9dcb5f25
Ryan Boll <bollr38@hotmail.com>
2024-12-06 19:20:16 -0600

    added query comments

--- member_funcs.py
+++ member_funcs.py
@@ -1,15 +1,18 @@
 def add_member(cursor, name, age, conn):
+    # Query to insert data and create a new member
     query = "INSERT INTO  Members (name, age) VALUES (%s, %s)"
     cursor.execute(query, (name, age))
     conn.commit()
 
 def update_member(cursor, new_age, member_id, conn):
+    # Query to update the age of a member by their id
     query = "UPDATE Members SET age = %s WHERE member_id = %s"
     cursor.execute(query,(new_age, member_id))
     conn.commit()
     print("Member age updated successfully.")
 
 def get_members_in_age_range(cursor, start_age, end_age):
+    # query to search database for members between specific ages
     query = "SELECT member_id, name, age FROM Members WHERE age BETWEEN %s AND %s"
     cursor.execute(query, (start_age, end_age))
     print(f"Here are the members between ages {start_age} and {end_age}:\n")
--- workout_funcs.py
+++ workout_funcs.py
@@ -1,12 +1,14 @@
 
 
 def add_workout(cursor, member_id, session_date, session_minutes, calories_burned, conn):
+    # Query to insert a new workout session into the database
     query = "INSERT INTO  WorkoutSessions (member_id, session_date, session_minutes, burned_calories) VALUES (%s, %s, %s, %s)"
     cursor.execute(query, (member_id, session_date, session_minutes, calories_burned))
     conn.commit()
 
 
 def delete_workout(cursor, session_id, member_id, conn):
+    # Query to find and delete a workout session based on the session_id and member_id
     query = "DELETE FROM WorkoutSessions WHERE session_id = %s AND member_id = %s"
     cursor.execute(query, (session_id, member_id))
     conn.commit()
